5193d6dde289eb7d75104e2dbc36215e3060150a
Jose Alvarez <jose.alvarez@hey.com>
2023-08-11 11:40:30 -0400

    Remove dead space in frame

--- utils.py
+++ utils.py
@@ -30,9 +30,37 @@ def show_frame(frame, delay=0):
     cv2.waitKey(delay)
 
 
+# this is not really suitable for production use
+boundary = None
+
+
+def crop(frame):
+    global boundary
+    if boundary is None:
+        # convert the image to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # threshold the image to get non-black pixels
+        _, threshold = cv2.threshold(gray, 40, 255, cv2.THRESH_BINARY)
+
+        # use opencv findContours to get bounding box of the non-black region
+        contours, _ = cv2.findContours(
+            threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
+        cnt = max(contours, key=cv2.contourArea)  # get the largest contour
+        x, y, w, h = cv2.boundingRect(cnt)
+        boundary = (x, y, w, h)
+    else:
+        (x, y, w, h) = boundary
+
+    frame = frame[y : y + h, x : x + w]
+    return frame
+
+
 # the default width and height here provide good results on our sample videos
 # but need to be tested on actual lab hardware
 def clamp_frame_size(frame, max_width=640, max_height=480):
+    frame = crop(frame)
     original_height, original_width = frame.shape[:2]
     aspect_ratio = original_width / original_height
 

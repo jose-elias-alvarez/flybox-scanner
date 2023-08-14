3df56a79e0c2c5640c4ec038c0a991a1ad694eb2
Jose Alvarez <jose.alvarez@hey.com>
2023-08-13 22:03:28 -0400

    Add webcam selection (WIP)

--- components/root_window.py
+++ components/root_window.py
@@ -3,17 +3,23 @@ from enum import Enum
 
 import cv2
 
-from components.cancel_button import CancelButton
 from components.capture_button import CaptureButton
 from components.hide_button import HideButton
 from components.main_canvas import MainCanvas
 from components.record_button import RecordButton
+from components.select_webcam_canvas import SelectWebcamCanvas
 
 
 class RootState(Enum):
     IDLE = "Idle"
     CAPTURING = "Capturing"
     RECORDING = "Recording"
+    SELECTING_WEBCAM = "Selecting Webcam"
+
+
+class CommandButton(tk.Button):
+    def __init__(self, window: "RootWindow", text: str, command):
+        super().__init__(window, text=text, command=command)
 
 
 class RootWindow(tk.Tk):
@@ -31,25 +37,41 @@ class RootWindow(tk.Tk):
         self.record_button = None
         self.cancel_button = None
 
-        self.cap = cv2.VideoCapture(0)
-
+        self._cap = cv2.VideoCapture(0)
         self.on_idle()
 
-    def on_idle(self):
+    # setter for cap that releases the previous cap
+    @property
+    def cap(self):
+        return self._cap
+
+    @cap.setter
+    def cap(self, cap):
+        if self._cap is not None:
+            self._cap.release()
+        self._cap = cap
+
+    def wipe(self):
         if self.canvas is not None:
             self.canvas.destroy()
         for component in self.winfo_children():
             component.destroy()
 
+    def on_idle(self):
+        self.wipe()
         self.canvas = MainCanvas(self, self.cap)
         self.canvas.pack()
         # visible elements
         self.capture_button = CaptureButton(self, self.canvas)
         self.capture_button.pack()
+        self.select_webcam_button = CommandButton(
+            self, "Select Webcam", self.on_selecting_webcam
+        )
+        self.select_webcam_button.pack()
         # hidden elements
         self.hide_button = HideButton(self, self.canvas)
         self.record_button = RecordButton(self, self.canvas)
-        self.cancel_button = CancelButton(self)
+        self.cancel_button = CommandButton(self, "Cancel", self.on_idle)
 
         self.state = RootState.IDLE
 
@@ -57,6 +79,7 @@ class RootWindow(tk.Tk):
         self.capture_button.config(text="Retry")
         self.record_button.pack()
         self.cancel_button.pack()
+        self.select_webcam_button.pack_forget()
         self.state = RootState.CAPTURING
 
     def on_recording(self):
@@ -64,6 +87,11 @@ class RootWindow(tk.Tk):
         self.record_button.pack_forget()
         self.hide_button.pack()
         self.state = RootState.RECORDING
+
+    def on_selecting_webcam(self):
+        self.wipe()
+        self.canvas = SelectWebcamCanvas(self)
+        self.state = RootState.SELECTING_WEBCAM
 
     def schedule_update(self):
         self.after(self.delay, self.update)
---
+++ components/select_webcam_canvas.py
@@ -0,0 +1,72 @@
+import tkinter as tk
+from typing import TYPE_CHECKING
+
+import cv2
+import PIL.Image
+import PIL.ImageTk
+
+if TYPE_CHECKING:
+    from components.root_window import RootWindow
+
+
+class SelectWebcamCanvas(tk.Canvas):
+    def __init__(self, window: "RootWindow"):
+        super().__init__(window, width=window.width, height=window.height)
+        self.pack()
+        self.window = window
+
+        self.sources = self.get_webcams()
+        self.current_source = self.sources[0]
+        self.cap = cv2.VideoCapture(self.current_source)
+        self.frame = None
+
+        self.selected_source = tk.StringVar()
+        self.selected_source.set("Webcam 0")  # initial value
+        self.dropdown = tk.OptionMenu(
+            self.window,
+            self.selected_source,
+            *["Webcam " + str(i) for i in self.sources],
+            command=self.change_webcam
+        )
+        self.dropdown.pack()
+
+        self.select_button = tk.Button(self.window, text="Select", command=self.select)
+        self.select_button.pack()
+
+    def get_webcams(self):
+        index = 0
+        arr = []
+        while index < 10:
+            cap = cv2.VideoCapture(index)
+            if not cap.read()[0]:
+                break
+            else:
+                arr.append(index)
+            cap.release()
+            index += 1
+        return arr
+
+    def change_webcam(self, selected_source):
+        self.current_source = int(selected_source.split(" ")[1])
+        self.cap.release()
+        self.cap = cv2.VideoCapture(self.current_source)
+
+    def resize(self, image):
+        (h, w) = image.shape[:2]
+        r = self.window.width / float(w)
+        dim = (self.window.width, int(h * r))
+
+        return cv2.resize(image, dim)
+
+    def select(self):
+        self.window.cap = self.cap
+        self.window.on_idle()
+
+    def update(self):
+        ret, frame = self.cap.read()
+        if ret:
+            frame = self.resize(frame)
+            self.frame = PIL.ImageTk.PhotoImage(
+                image=PIL.Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            )
+            self.create_image(0, 0, image=self.frame, anchor=tk.NW)
